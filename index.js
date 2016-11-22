c1db37d1b07c535596fe84b66cf1169a3b7cc7f7
Maksim <maksim@wisestamp.com>
2016-11-22 16:46:00 +0200

    fix index.js routes

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use("/styles",  express.static(__dirname + '/styles'));
 app.use("/app", express.static(__dirname + '/app'));
 app.use("/lib",  express.static(__dirname + '/lib'));
 app.use("/images",  express.static(__dirname + '/images'));
-app.use("/app/tpl",  express.static(__dirname + '/app/tpl'));
 
 app.get('/', function (req, res) {
       res.sendfile(__dirname + '/index.html');
